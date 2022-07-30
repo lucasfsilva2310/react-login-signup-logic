f189719a119e5270750e6699b6904b6258ab3029
lucas <lucas.f.silva@provi.com.br>
2022-07-30 15:07:20 -0300

    npx create-react-app --template typescript runned to create app from scratch. Adding react-hook-form, yup and @hookform/resolvers libraries to App. Creating first version of both login and signUp forms validations

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as yup from 'yup'
+
+const loginFormSchema = yup.object().shape({
+  username: yup.string().required('Este campo é obrigatório'),
+  password: yup.string().required('Este campo é obrigatório'),
+})
+
+const signUpFormSchema = yup.object().shape({
+  username: yup.string().required('Este campo é obrigatório'),
+  password: yup.string().required('Este campo é obrigatório'),
+})
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div></div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
