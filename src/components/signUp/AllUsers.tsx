44dcb1bc43cfff4301fb34f9a1afa61ef1f70146
lucas <lucas.f.silva@provi.com.br>
2022-07-30 19:53:35 -0300

    adding minimum CSS to improve layour

--- src/components/signUp/AllUsers.tsx
+++ src/components/signUp/AllUsers.tsx
@@ -1,18 +1,23 @@
-import { userProps, usersListProps } from '../../types/user'
+import styled from 'styled-components'
+import { userProps, usersList } from '../../types/user'
 
-export const AllUsers = ({ users }: usersListProps) => {
+const UserCard = styled.div`
+  margin: 10px;
+`
+
+export const AllUsers = ({ users }: usersList) => {
   return (
     <div>
       <>
         <span>Todos os usuários existentes:</span>
         {users &&
-          users.map((user: userProps, index) => {
+          users.map((user: userProps) => {
             return (
-              <div key={user.id}>
-                <span>ID: {user.id}</span>
-                <span>Nome: {user.username}</span>
-                <span>Senha: {user.password}</span>
-              </div>
+              <UserCard key={user.id}>
+                <p>ID: {user.id}</p>
+                <p>Nome: {user.username}</p>
+                <p>Senha: {user.password}</p>
+              </UserCard>
             )
           })}
       </>
