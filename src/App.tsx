c52c6ccdbc0104f27e0a1afc34967cff9814c3a7
lucas <lucas.f.silva@provi.com.br>
2022-07-30 16:17:22 -0300

    Isolating signUp code to a new component called SignUp and changing english names to portuguese names in the UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,11 @@
-import * as yup from 'yup'
-import { useForm } from 'react-hook-form'
-import { yupResolver } from '@hookform/resolvers/yup'
 import { Login } from './components/login'
-
-const signUpFormSchema = yup.object().shape({
-  username: yup.string().required('Este campo é obrigatório'),
-  password: yup.string().required('Este campo é obrigatório'),
-  passwordConfirmation: yup
-    .string()
-    .oneOf([yup.ref('password'), null], 'As senhas têm que ser iguais!'),
-})
+import { SignUp } from './components/signUp'
 
 function App() {
-  const {
-    handleSubmit: handleSignUpForm,
-    control: controlSignUp,
-    formState: { errors: errorsSignUp },
-  } = useForm<{ username: string; password: string }>({
-    resolver: yupResolver(signUpFormSchema),
-  })
-
   return (
     <div className="App">
       <Login />
+      <SignUp />
     </div>
   )
 }
--- src/components/signUp/index.tsx
+++ src/components/signUp/index.tsx
@@ -2,45 +2,48 @@ import * as yup from 'yup'
 import { useForm } from 'react-hook-form'
 import { yupResolver } from '@hookform/resolvers/yup'
 
-const loginFormSchema = yup.object().shape({
+const signUpFormSchema = yup.object().shape({
   username: yup.string().required('Este campo é obrigatório'),
   password: yup.string().required('Este campo é obrigatório'),
+  passwordConfirmation: yup
+    .string()
+    .oneOf([yup.ref('password'), null], 'As senhas têm que ser iguais!'),
 })
 
-type loginSubmitFormData = {
+type signUpSubmitFormData = {
   username: string
   password: string
 }
 
-type formHandlerProps = {
+type signUpFormHandlerProps = {
   username: string
   password: string
+  passwordConfirmation: string
 }
 
-export const Login = () => {
+export const SignUp = () => {
   const {
-    register,
     handleSubmit,
+    register,
     formState: { errors },
-  } = useForm<formHandlerProps>({
-    resolver: yupResolver(loginFormSchema),
+  } = useForm<signUpFormHandlerProps>({
+    resolver: yupResolver(signUpFormSchema),
   })
 
-  const loginFormHandler = (data: loginSubmitFormData) => {
+  const signUpFormHandler = (data: signUpSubmitFormData) => {
     const payload = {
       username: data.username,
       password: data.password,
     }
 
     console.log(payload)
-    // Checar se o Usuário existe (adicionar lib testServer da rocketSeat)
+    // Cadastrar usuário no servidor
   }
 
   return (
-    <form onSubmit={handleSubmit(loginFormHandler)}>
+    <form onSubmit={handleSubmit(signUpFormHandler)}>
       <div>
         <div>
-          {' '}
           <span>Usuário</span>
           <input type="text" {...register('username')} />
           <span>{errors.username?.message || ''}</span>
@@ -50,10 +53,12 @@ export const Login = () => {
           <input type="text" {...register('password')} />
           <span>{errors.password?.message || ''}</span>
         </div>
-        <button type="submit">Entrar</button>
-        <button type="button" onClick={() => console.log('Change Route')}>
-          Cadastrar
-        </button>
+        <div>
+          <span>Confirmação de senha</span>
+          <input type="text" {...register('passwordConfirmation')} />
+          <span>{errors.passwordConfirmation?.message || ''}</span>
+        </div>
+        <button type="submit">Cadastrar</button>
       </div>
     </form>
   )
