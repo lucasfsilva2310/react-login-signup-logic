19689e183acae5976d30c31eb03bc64eed7cf65c
lucas <lucas.f.silva@provi.com.br>
2022-07-30 16:27:13 -0300

    adding react-router-dom library and creating new routes for both default path ( login ) and signUp path

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import { Login } from './components/login'
 import { SignUp } from './components/signUp'
 
+import { Route, Routes } from 'react-router-dom'
+
 function App() {
   return (
     <div className="App">
-      <Login />
-      <SignUp />
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
     </div>
   )
 }
